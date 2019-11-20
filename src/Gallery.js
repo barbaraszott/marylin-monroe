6ed65a32fc9b0f7406c2398450a51fd16202c3be
Barbara Szott <barbara.aleksy@gmail.com>
2019-11-20 08:59:07 +0100

    Create Gallery and User components

---
+++ src/Gallery.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Gallery extends Component {
+	static propTypes = {};
+
+	render() {
+		return <h1>Gallery, wohooo!</h1>;
+	}
+}
+
+export default Gallery;
