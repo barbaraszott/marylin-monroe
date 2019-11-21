e3d8b304935f6f775a7db9f277807a759856647a
Barbara Szott <barbara.aleksy@gmail.com>
2019-11-21 13:05:16 +0100

    Change Hedaer to function component

--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,27 @@
-import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import './Header.scss';
 
-export class Header extends PureComponent {
-	render() {
-		return (
-			<React.Fragment>
-				<header className="header">
-					<div className="user-photo" />
-				</header>
-				<nav className="navbar">
-					{/* TODO: read more about exact */}
-					<NavLink exact to="/" className="navbar__link" activeClassName="navbar__link--selected">
-						<i className="fas fa-user fa-lg" />
-					</NavLink>
-					<NavLink to="/gallery" className="navbar__link" activeClassName="navbar__link--selected">
-						<i className="far fa-image fa-lg" />
-					</NavLink>
-				</nav>
-			</React.Fragment>
-		);
-	}
+function Header() {
+	return (
+		<React.Fragment>
+			<header className="header">
+				<div className="user-photo" />
+			</header>
+			<nav className="navbar">
+				{/* TODO: read more about exact */}
+				<NavLink exact to="/" className="navbar__link" activeClassName="navbar__link--selected">
+					<i className="fas fa-user fa-lg" />
+				</NavLink>
+				<NavLink to="/gallery" className="navbar__link" activeClassName="navbar__link--selected">
+					<i className="far fa-image fa-lg" />
+				</NavLink>
+			</nav>
+		</React.Fragment>
+	);
 }
 
 Header.propTypes = {};
