a90dca6ff779fd293d70f88fab6efa529653c6cf
Barbara Szott <barbara.aleksy@gmail.com>
2019-11-21 17:51:09 +0100

    Create User component as function component

--- src/User.js
+++ src/User.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-export class User extends Component {
-	static propTypes = {};
+import './user.scss';
 
-	render() {
-		return <h1>User, yay!</h1>;
-	}
+function User() {
+	return (
+		<React.Fragment>
+			<h1 className="name">Marilyn Monroe</h1>
+			<span className="localization">Poznan, PL</span>
+			<section className="description">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+					magna aliqua. Eget duis at tellus at urna condimentum mattis pellentesque. Interdum consectetur libero id
+					faucibus nisl.
+				</p>
+				<blockquote className="description__quote">
+					I believe that everything happens for a reason. People change so that you can learn to let go, things go wrong
+					so that you appreciate them when they’re right, you believe lies so you eventually learn to trust no one but
+					yourself, and sometimes good things fall apart so better things can fall together.
+				</blockquote>
+				<p>
+					Adipiscing bibendum est ultricies integer quis. Bibendum enim facilisis gravida neque convallis a cras semper
+					auctor. Volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Sapien pellentesque
+					habitant morbi tristique senectus.
+				</p>
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default User;
