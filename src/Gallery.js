9e765a0441be53bee0c6124c177fb39592bb04f9
Barbara Szott <barbara.aleksy@gmail.com>
2019-11-21 12:26:36 +0100

    Fetch Flickr API data, map it to photos, dispatch photos

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import jsonp from 'jsonp';
 import PropTypes from 'prop-types';
 
+import './gallery.scss';
 export class Gallery extends Component {
 	static propTypes = {};
+	componentDidMount() {
+		if (this.props.photos.length !== 0) return;
+
+		const TAGS = [
+			'marilyn',
+			'monroe'
+		].join();
+
+		const PHOTOS_TO_SHOW = 9;
+
+		const url = `https://www.flickr.com/services/feeds/photos_public.gne?format=json&tags=${TAGS}`;
+
+		window.jsonFlickrFeed = (response) => {
+			const photos = response.items.slice(0, PHOTOS_TO_SHOW).map((item) => ({
+				title    : item.title,
+				author   : item.author.match(/\("(.*)"\)/)[1],
+				// author      : item.author.split(/\("|"\)/)[1],
+				imageSrc : item.media.m,
+				link     : item.link
+			}));
+
+			this.props.dispatch({
+				type   : 'PHOTOS_FETCHED',
+				photos
+			});
+		};
+
+		jsonp(url);
+	}
 
 	render() {
 		return <h1>Gallery, wohooo!</h1>;
