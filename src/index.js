9fc574e9a8849da388d391cdde2d6ac5e46ab9f4
Barbara Szott <barbara.aleksy@gmail.com>
2019-11-27 18:02:00 +0100

    Install redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 const initialState = {
@@ -19,7 +20,7 @@ function reducer(state = initialState, action) {
 	}
 }
 
-let store = createStore(reducer);
+let store = createStore(reducer, applyMiddleware(thunk));
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
