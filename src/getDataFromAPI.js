5081e43de11c5475792bedab91c9bfd93f7c9530
Barbara Szott <barbara.aleksy@gmail.com>
2019-11-28 12:10:02 +0100

    Add different version of tags for fetching photos

--- src/getDataFromAPI.js
+++ src/getDataFromAPI.js
@@ -6,17 +6,22 @@ const getDataFromAPI = () => {
 	return function getPhotos(dispatch) {
 		const PHOTOS_TO_SHOW = 9;
 		const TAGS = [
-			'marilyn',
-			'monroe'
+			// 'marilyn',
+			// 'monroe',
+			'marylin monroe'
 		].join();
 
-		const url = `https://www.flickr.com/services/feeds/photos_public.gne?format=json&tags=${TAGS}`;
+		// TAGMODE:
+		// set to 'any' to show photos with any tag from the tags' list
+		// set to 'all' to show photos that have all the tags
+		const TAGMODE = 'any';
+
+		const url = `https://www.flickr.com/services/feeds/photos_public.gne?format=json&tags=${TAGS}&tagmode=${TAGMODE}`;
 
 		window.jsonFlickrFeed = (response) => {
 			const photos = response.items.slice(0, PHOTOS_TO_SHOW).map((item) => ({
 				title    : item.title,
 				author   : item.author.match(/\("(.*)"\)/)[1],
-				// author      : item.author.split(/\("|"\)/)[1],
 				imageSrc : item.media.m,
 				link     : item.link
 			}));
