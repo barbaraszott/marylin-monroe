85035492e1768f02eb9841671d44df49e8ce275a
Barbara Szott <barbara.aleksy@gmail.com>
2019-11-28 12:31:12 +0100

    Updates

---
+++ precache-manifest.556fa42a4ae2d2ca73d22b384be0c5c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25250978a13fc445cfb1aeeb61e1d35",
+    "url": "/marylin-monroe/index.html"
+  },
+  {
+    "revision": "923010abbd7197ea402b",
+    "url": "/marylin-monroe/static/css/main.f59b79b4.chunk.css"
+  },
+  {
+    "revision": "341788c545f49c9b5c67",
+    "url": "/marylin-monroe/static/js/2.9a0ecb4e.chunk.js"
+  },
+  {
+    "revision": "923010abbd7197ea402b",
+    "url": "/marylin-monroe/static/js/main.32823032.chunk.js"
+  },
+  {
+    "revision": "8a8cdee3ecd628b8deb7",
+    "url": "/marylin-monroe/static/js/runtime-main.f718f768.js"
+  },
+  {
+    "revision": "930803444c4f49f37d272cdafb93b988",
+    "url": "/marylin-monroe/static/media/sand-bcg.93080344.jpg"
+  },
+  {
+    "revision": "bf9c170af7ab4f9148ea0ada296a9361",
+    "url": "/marylin-monroe/static/media/user-photo.bf9c170a.jpg"
+  }
+]);
